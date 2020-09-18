99acd79143b68fb298720480deed4375c5ea52d7
ZF4 <zfarmer313@gmail.com>
2020-09-18 17:17:31 -0500

    finished DOM

---
+++ app.js
@@ -0,0 +1,93 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let btn = document.createElement('button');
+    btn.innerHTML = "Add square";
+    let allDiv = document.createElement('div');
+    document.body.appendChild(btn);
+    document.body.appendChild(allDiv);
+    let squareId = 1;
+    document.body.style.backgroundColor = "#6d6875";
+
+    btn.addEventListener('click', function () {
+        let square = document.createElement('div');
+        let text = document.createTextNode(squareId);
+        square.style.color = "white";
+        square.appendChild(text);
+        let squareText = document.createElement("h1");
+        square.appendChild(squareText);
+        squareText.appendChild(text);
+        // squareText.style.display = "none"
+        square.className = "square";
+        square.id = squareId
+        square.style.border = "thick solid white";
+        square.style.background = "black";
+        square.style.height = "100px";
+        square.style.width = "100px"
+        square.style.float = "left"
+        square.style.flexDirection = "row";
+        square.style.flexWrap = "wrap";
+        allDiv.appendChild(square);
+        squareId++;
+
+        square.addEventListener("mouseover", function () {
+            squareText.style.display = "block";
+
+        })
+        square.addEventListener("mouseout", function () {
+            squareText.style.display = "none"
+
+        })
+
+        square.addEventListener("click", function () {
+            let colors = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"]
+            let randomNum = Math.floor(Math.random() * colors.length)
+            square.style.backgroundColor = colors[randomNum]
+
+
+        })
+
+        square.addEventListener("dblclick", function () {
+            if (square.id % 2 == 0) {
+                if (square.nextElementSibling) {
+                    square.nextElementSibling.remove()
+                } else {
+                    alert("No square to the right!")
+                }
+            }
+            else if (square.id % 2 == 1) {
+                if (square.previousElementSibling) {
+                    square.previousElementSibling.remove()
+                } else {
+                    alert("No square to the left!")
+                }
+            }
+
+        })
+
+
+
+    })
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
